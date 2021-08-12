f3419876963cb5103cb7cf6eaadfd3898b6c80e5
Nierot <git@nierot.com>
2021-08-12 10:22:07 +0200

    ✨ See all playlists

--- src/pages/home/Playlists.tsx
+++ src/pages/home/Playlists.tsx
@@ -1,9 +1,12 @@
-import { IonContent, IonPage } from '@ionic/react'
+import { IonButton, IonContent, IonIcon, IonItem, IonLabel, IonList, IonPage } from '@ionic/react'
 import { useState, useEffect, useContext } from 'react'
 import { AuthContext } from '../../context/FirebaseAuthContext'
 import { database } from '../../firebase'
 import firebase from 'firebase'
 import './Playlists.css'
+import { arrowBack } from 'ionicons/icons'
+import { redirect } from '../../util'
+import { getPlaylists } from '../../storage/playlist'
 
 const Playlists = () => {
 
@@ -12,41 +15,62 @@ const Playlists = () => {
   const playlistRef = database.ref(`users/${user.user?.uid}/playlists`)
 
   useEffect(() => {
-    playlistRef
-      .get()
-      .then(snap => {
-        let list: Object[] = []
-        snap.forEach(p => { list.push(p.val()) })
-        return list
-      })
-      .then(setPlaylists)
-      .catch(err => {
-        console.error(err)
-        alert(err)
-      })
-  }, [])
-
-  const createNewPlaylist = (name: string) => {
-    let newList = playlistRef.push(name)
-
-    newList.set({
-      name,
-      dateCreated: new Date().toISOString()
-    })
+    (async () => setPlaylists(await getPlaylists(user.user!.uid)))()
+    // playlistRef
+    //   .get()
+    //   .then(snap => {
+    //     let list: Object[] = []
+    //     snap.forEach(p => { list.push(p.val()) })
+    //     return list
+    //   })
+    //   .then(setPlaylists)
+    //   .catch(err => {
+    //     console.error(err)
+    //     alert(err)
+    //   })
+  }, [user.user])
+  
+  const generateList = () => {
+    return <IonList color="none">
+      { playlists.map(a => <IonItem key={a.dateCreated}>
+          <IonButton slot="end" onClick={() => alert('TODO')}>
+            View
+          </IonButton>
+          <IonLabel>
+            <h2>{a.name}</h2>
+            <h3>{new Date(a.dateCreated).toLocaleString('nl')}</h3>
+          </IonLabel>
+        </IonItem>)}
+    </IonList>
   }
 
-  const generateList = () => {
-    return <div>
-      { playlists.map(a => <div>
-          <h1>{a.name}</h1>
-          <h2>{a.dateCreated}</h2>
-        </div>)}
-    </div>
+  const click = () => {
+    redirect('/newplaylist')
   }
 
   return <IonPage>
     <IonContent>
-      { generateList() }
+      <div id="playlists" className="background">
+        <div className="header width-90">
+          <a href="/home" className="back">
+            <IonIcon icon={arrowBack} className="back-icon"/>
+            <span>Back</span>
+          </a>
+        </div>
+        <div id="playlists-content">
+          <h1>
+            My playlists
+          </h1>
+          <div id="btn">
+            <IonButton onClick={click}>
+              Add New
+            </IonButton>
+          </div>
+        </div>
+        <div id="playlists-list">
+          { generateList() }
+        </div>
+      </div>
     </IonContent>
   </IonPage>
 }
